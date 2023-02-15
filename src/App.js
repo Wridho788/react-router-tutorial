48c16408750567caca27965adc22305767db8937
Wridho788 <wridho246@gmail.com>
2023-02-15 15:48:45 +0700

    add dynamic route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import FeaturedProducts from "./components/FeaturedProducts";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import NewProducts from "./components/NewProducts";
-import NotFound from "./components/NotFound";
-import OrderSummary from "./components/OrderSummary";
-import Products from "./components/Products";
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import FeaturedProducts from './components/FeaturedProducts';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import NewProducts from './components/NewProducts';
+import NotFound from './components/NotFound';
+import OrderSummary from './components/OrderSummary';
+import Products from './components/Products';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="order-summary" element={<OrderSummary />} />
-        <Route path="products" element={<Products />}>
-          <Route path="featured" element={<FeaturedProducts />} />
-          <Route path="new" element={<NewProducts />} />
-
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />} />
+        <Route path='products' element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
+          <Route path='featured' element={<FeaturedProducts />} />
+          <Route path='new' element={<NewProducts />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
-
   );
 }
 
